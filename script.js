8e43cb9c826f11b29b19bdc9529ed0da4039fc3b
Glitch (hello-webpage) <none>
2019-12-07 04:43:40 +0000

    🚝🍋 Checkpoint ./index.html:7638468/129 ./script.js:7638468/709 ./.glitch-assets:7638468/370

--- script.js
+++ script.js
@@ -4,8 +4,22 @@ var img = new Image();
 let canvas = null;
 let context2D = null;
 
+const handleStart = (event) => {
+  
+}
+
+const loadImage = () => {
+  const file = 'https://cdn.glitch.com/e3d07aa6-332d-4c23-83e2-1bb0fee35f02%2Ftestimage.jpg?v=1575693604586';
+  const response = fetch(file);
+  
+}
+
 const setup = async () => {
   canvas = document.getElementByID('image');
   context2D = canvas.getContext('2d');
+  
+  loadImage();
+  const buttonElement = document.querySelector('#start');
+  buttonElement.addEventListener('click', handleStart, {once: true});
 }
 window.addEventListener('load', setup, {once: true});
