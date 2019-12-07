3450778ebfacbc5a64fce085a7ae7b6405153fbb
Glitch (hello-webpage) <none>
2019-12-07 04:54:43 +0000

    🚕🗽 Checkpoint ./script.js:7638468/1359

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 const context = new AudioContext();
-var img = new Image();
+const imageData = null;
 
 let canvas = null;
 let context2D = null;
 
 const handleStart = (event) => {
-  
+  const image = new Image();
+  image.src = 'https://cdn.glitch.com/e3d07aa6-332d-4c23-83e2-1bb0fee35f02%2Ftestimage.jpg?v=1575693604586';
+  image.onload = context2D.drawImage(image, 0, 0)
+  const imageData = context2D.getImageData(0, 0, 200, 200);
 }
 
-const loadImage = () => {
-  const file = 'https://cdn.glitch.com/e3d07aa6-332d-4c23-83e2-1bb0fee35f02%2Ftestimage.jpg?v=1575693604586';
-  const response = fetch(file);
-  
-}
 
 const setup = async () => {
   canvas = document.getElementByID('image');
   context2D = canvas.getContext('2d');
   
-  loadImage();
+  //const file = 'https://cdn.glitch.com/e3d07aa6-332d-4c23-83e2-1bb0fee35f02%2Ftestimage.jpg?v=1575693604586';
+  //const response = await fetch(file);
+  
+  
+  
   const buttonElement = document.querySelector('#start');
   buttonElement.addEventListener('click', handleStart, {once: true});
 }
