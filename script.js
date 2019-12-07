06cd150a3150c9aac8334c3957d224b8a1f38eda
Glitch (hello-webpage) <none>
2019-12-07 05:06:40 +0000

    🌍🌾 Checkpoint ./script.js:7638468/280

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 const context = new AudioContext();
-const imageData = null;
+const imageData = [];
+const image = new Image();
 
 let canvas = null;
 let context2D = null;
 
 const handleStart = (event) => {
-  const image = new Image();
   image.src = 'https://cdn.glitch.com/e3d07aa6-332d-4c23-83e2-1bb0fee35f02%2Ftestimage.jpg?v=1575693604586';
-  image.onload = context2D.drawImage(image, 0, 0)
-  const imageData = context2D.getImageData(0, 0, 200, 200);
+  image.onload();
+  //context2D.drawImage(image, 0, 0)
+  imageData = context2D.getImageData(0, 0, 200, 200);
+  console.log(imageData);
+}
+
+image.onload = function() {
+  context2D.drawImage(image, 0, 0);
 }
 
 
 const setup = async () => {
-  canvas = document.getElementByID('image');
+  canvas = document.getElementById('image');
   context2D = canvas.getContext('2d');
   
   //const file = 'https://cdn.glitch.com/e3d07aa6-332d-4c23-83e2-1bb0fee35f02%2Ftestimage.jpg?v=1575693604586';
   //const response = await fetch(file);
-  
-  
   
   const buttonElement = document.querySelector('#start');
   buttonElement.addEventListener('click', handleStart, {once: true});
