b1dea3bf5ffa173ee14e3faf8e34ec8912c2e034
Glitch (hello-webpage) <none>
2019-12-08 01:19:45 +0000

    👢🎄 Checkpoint ./script.js:7638468/956

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const context = new AudioContext();
-//var imageData = [];
+
 const image = new Image();
 var red = [];
 var green = [];
 var blue = [];
 var alpha = [];
+var imageData = new ImageData(200, 200);
 
 let canvas = null;
 let context2D = null;
@@ -14,27 +15,19 @@ let context2D = null;
 const handleStart = (event) => {
   image.src = 'https://cdn.glitch.com/e3d07aa6-332d-4c23-83e2-1bb0fee35f02%2Ftestimage.jpg?v=1575693604586';
   image.onload();
-  let imageData = context2D.getImageData(0, 0, 200, 200);
-  
-  //imageData = context2D.getImageData(0, 0, 200, 200).data;
+  //let imageData = context2D.getImageData(0, 0, 200, 200);
   console.log(imageData);
   
   let x = 0;
   let y = 0;
   let i = 0;
-  //for(let i = 0; i < imageData.data.length; i += 4) {
-  // console.log(context2D[x,y, 1, 1]);
-  //   const pixelData = image[x,y].getImageData(x, y, 1, 1).data;
-  //   red.push(pixelData[i]);
-  //   green.push(pixelData[i+1]);
-  //   blue.push(pixelData[i+2]);
-  //   alpha.push(pixelData[i+3]);
- //}
-  // console.log(red);
+
 }
 
 image.onload = function() {
   context2D.drawImage(image, 0, 0);
+  console.log(context2D.getImageData(0, 0, 200, 200).data);
+  
 }
 
 
