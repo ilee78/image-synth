224efacbfdccdba6df5cafe49442e6fda0b7e57e
Glitch (hello-webpage) <none>
2019-12-08 07:07:42 +0000

    🚴🔑 Checkpoint ./SoundModule.js:7638468/694

--- SoundModule.js
+++ SoundModule.js
@@ -1,4 +1,5 @@
-import Metronome from 
+import Metronome from './Metronome.js';
+
 class SoundModule {
   constructor(context, r, g, b, a) {
 
@@ -6,6 +7,9 @@ class SoundModule {
     this._osc = new OscillatorNode(this._context);
     this._amp = new GainNode(this._context);
     this._osc.type = 'sine';
+    
+    this._metro = new Metronome(this._context);
+    this._metro.setBPM(100);
     
     this.output = new GainNode(this._context);
     this._osc.connect(this._amp).connect(this.output);
@@ -15,22 +19,31 @@ class SoundModule {
   }
   
   play(r) {
-    const now = this._context.currentTime;
-    const later = now + 10;
-    let i = 0;
-    //console.log(r);
-    
-    while(i < r.length) {
-      const pitch = r[i];
-      console.log(r[i]);
-      this._notePitch = pitch * 10;
-      // console.log(this._notePitch);
-      // console.log(later);
-      // console.log(pitch);
-      this._osc.frequency.linearRampToValueAtTime(this._notePitch, later);
-      i++;
-      //if(i > 100) break;
+    console.log("hello");
+    this._metro.start();
+    this._metro.onbeat = (start, interval, counter) => {
+      if(counter > 400) return;
+      console.log(counter);
+      this._notePitch = r[counter] * 10;
+      console.log(this._notePitch);
+      this._osc.frequency.exponentialRampToValueAtTime(this._notePitch, this._context.currentTime + 0.5);     
     }
+//     const now = this._context.currentTime;
+//     const later = now + 10;
+//     let i = 0;
+//     //console.log(r);
+    
+//     while(i < r.length) {
+//       const pitch = r[i];
+//       console.log(r[i]);
+//       this._notePitch = pitch * 10;
+//       // console.log(this._notePitch);
+//       // console.log(later);
+//       // console.log(pitch);
+//       this._osc.frequency.linearRampToValueAtTime(this._notePitch, later);
+//       i++;
+//       //if(i > 100) break;
+//     }
     //console.log(r);
   }
 }
