927fb676e71896e26b6b0b9e03024c042911f1f5
Glitch (hello-webpage) <none>
2019-12-08 00:45:42 +0000

    📮📹 Checkpoint ./index.html:7638468/23 ./script.js:7638468/141

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const context = new AudioContext();
-var imageData = [];
+//var imageData = [];
 const image = new Image();
 
 let canvas = null;
 let context2D = null;
+let imageData = new ImageData(200, 200);
 
 const handleStart = (event) => {
   image.src = 'https://cdn.glitch.com/e3d07aa6-332d-4c23-83e2-1bb0fee35f02%2Ftestimage.jpg?v=1575693604586';
   image.onload();
-  //context2D.drawImage(image, 0, 0)
-  imageData = context2D.getImageData(0, 0, 200, 200);
+  imageData = context2D.getImageData(0, 0, 200, 200).data;
   console.log(imageData);
 }
 
