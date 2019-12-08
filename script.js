527a551495af0738c9092ba13ab31c9b38cc6906
Glitch (hello-webpage) <none>
2019-12-08 06:17:47 +0000

    🏥🙍 Checkpoint ./script.js:7638468/1223 ./index.html:7638468/36

--- script.js
+++ script.js
@@ -8,20 +8,23 @@ const r = [];
 const g = [];
 const b = [];
 const a = [];
-//var imageData = [];
+var imageData = [];
 
 let canvas = null;
 let context2D = null;
 
 const handleStart = (event) => {
-  image.src = 'https://cdn.glitch.com/e3d07aa6-332d-4c23-83e2-1bb0fee35f02%2Ftestimage.jpg?v=1575693604586';
-  window.onload();
+  console.log(imageData);
+  createPixelArrays(imageData);
+  //image.src = 'https://cdn.glitch.com/e3d07aa6-332d-4c23-83e2-1bb0fee35f02%2Ftestimage.jpg?v=1575693604586';
+  //image.onload();
 }
+
 
 image.onload = function() {
   image.crossOrigin = "Anonymous";
   context2D.drawImage(image, 0, 0);
-  const imageData = context2D.getImageData(0, 0, 200, 200).data;
+  imageData = context2D.getImageData(0, 0, 200, 200).data;
   console.log(imageData);
   // createPixelArrays(imageData); 
 }
@@ -81,8 +84,12 @@ const setup = async () => {
   context2D = canvas.getContext('2d');
   
   const buttonElement = document.querySelector('#start');
+  
+  image.src = 'https://cdn.glitch.com/e3d07aa6-332d-4c23-83e2-1bb0fee35f02%2Ftestimage.jpg?v=1575693604586';
+  image.crossOrigin = "Anonymous";
+  image.onload();
+  
   buttonElement.addEventListener('click', handleStart, {once: true});
-  const loadButton = document.querySelector('#load');
-  //loadButton.addEventListener('click', handleLoad, {once: true});
+
 }
 window.addEventListener('load', setup, {once: true});
