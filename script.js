2f59b88b775e5bbd243b6ee1ecafbdbc9dee291b
Glitch (hello-webpage) <none>
2019-12-08 03:55:46 +0000

    🤑🐽 Checkpoint ./script.js:7638468/1276

--- script.js
+++ script.js
@@ -9,26 +9,26 @@ var imageData = new ImageData(200, 200);
 
 let canvas = null;
 let context2D = null;
-//let imageData = new ImageData(200, 200);
-//const imageData = context2D.createImageData(200, 200);
 
 const handleStart = (event) => {
   image.src = 'https://cdn.glitch.com/e3d07aa6-332d-4c23-83e2-1bb0fee35f02%2Ftestimage.jpg?v=1575693604586';
-  //let imageData = context2D.getImageData(0, 0, 200, 200);
-  image.crossOrigin = "Anonymous";
+  //image.crossOrigin = "Anonymous";
   image.onload();
-  console.log(imageData);
+  //imageData = context2D.getImageData(0, 0, 200, 200).data;
+  //console.log(imageData);
   
   let x = 0;
   let y = 0;
-  let i = 0;
-
+  //for(let i = 0; i < imageData.data.length; i += 4) {
+  const i = imageData[2];
+  console.log(imageData);
 }
 
 image.onload = function() {
+  image.crossOrigin = "Anonymous";
   context2D.drawImage(image, 0, 0);
-  console.log(context2D.getImageData(0, 0, 200, 200).data);
-  
+  imageData = context2D.getImageData(0, 0, 200, 200).data;
+  //console.log(imageData);  
 }
 
 
@@ -36,8 +36,6 @@ const setup = async () => {
   canvas = document.getElementById('image');
   context2D = canvas.getContext('2d');
   
-  //const file = 'https://cdn.glitch.com/e3d07aa6-332d-4c23-83e2-1bb0fee35f02%2Ftestimage.jpg?v=1575693604586';
-  //const response = await fetch(file);
   
   const buttonElement = document.querySelector('#start');
   buttonElement.addEventListener('click', handleStart, {once: true});
